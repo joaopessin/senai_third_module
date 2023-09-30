59e386f96537382506273d9d8290e6cebc9da3cd
joaopessin <jv.pessin20@gmail.com>
2023-09-29 21:40:14 -0300

    added 09_29 iot folder

---
+++ IoT/09_29/12_code.c
@@ -0,0 +1,779 @@
+int buzzer = 2;
+int time = 250;
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop() {
+  // HE'S A PIRATE
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // -------------------------
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // FA#
+  tone(buzzer, 370, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // -------------------------
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // -------------------------
+
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // RE#
+  tone(buzzer, 311, 300);
+  delay(time);
+
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // FA / F
+  tone(buzzer, 349, 300);
+  delay(time);
+  // RE / D
+  tone(buzzer, 294, 300);
+  delay(time);
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+
+  // DO / C
+  tone(buzzer, 262, 200);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // LA#
+  tone(buzzer, 467, 300);
+  delay(time);
+  // LA / A
+  tone(buzzer, 440, 300);
+  delay(time);
+  // SOL / G
+  tone(buzzer, 392, 300);
+  delay(time);
+}
